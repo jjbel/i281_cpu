13f7b259fc1ad0a18a0217dd493135e2a85a091a
Shridhar Patil <Shridhar.g.patil1@gmail.com>
2025-11-20 16:15:57 +0530

    data and instruction memory made

--- i281_toy_main/Code Memory/codemem.v
+++ i281_toy_main/Code Memory/codemem.v
@@ -1,3 +1,46 @@
-module codemem ();
+/*
+Code memory (64 instuctions, 16 bits each)
+
+Specifications: 
+c1: write enable bit
+write_select: 6 bit register to select which address to write into
+inp: input instruction for said address
+read_select: 6 bit register to read from
+curr_instruction: output 16 bit instruction 
+clock and reset: clock input and reset
+
+code_memory_reg: internal 64 registers of 16 bits 
+*/
+
+module codemem (
+    input clock,
+    input reset,
+    input c1,
+    input [5:0] write_select,
+    input [15:0] inp,
+    input [5:0] read_select,
+    output reg [15:0] curr_instruction
+);
+
+integer i; //for loops 
+
+reg [15:0] code_memory_reg[63:0];
+
+always@(posedge clock or posedge reset)
+begin
+    if(reset)
+    begin
+        for(i = 0; i < 64; i = i+1)
+            code_memory_reg[i] <= 16'b0; //reset all to 0
+    end
+    else 
+    begin
+        curr_instruction <= code_memory_reg[read_select];
+        if(c1)
+        begin
+            code_memory_reg[write_select] <= inp;
+        end
+    end
+end
 
 endmodule
---
+++ i281_toy_main/Data Memory/datamem.v
@@ -0,0 +1,46 @@
+/*
+Data memory (64 instuctions, 16 bits each)
+
+Specifications: 
+c17: write enable bit
+write_select: 4 bit register to select which address to write into
+inp: input data for said address
+read_select: 4 bit register to read from
+data_memory_output: output 8 bit 
+clock and reset: clock input and reset
+
+data_memory_reg: internal 16 registers of 8 bits 
+*/
+
+module datamem (
+    input clock,
+    input reset,
+    input c17,
+    input [3:0] write_select,
+    input [7:0] inp,
+    input [3:0] read_select,
+    output reg [7:0] data_memory_output
+);
+
+integer i; //for loops
+
+reg [7:0] data_memory_reg[15:0];
+
+always@(posedge clock or posedge reset)
+begin
+    if(reset)
+    begin
+        for(i = 0; i < 16; i = i+1)
+            data_memory_reg[i] <= 8'b0; //reset all to 0
+    end
+    else 
+    begin
+        data_memory_output <= data_memory_reg[read_select];
+        if(c17)
+        begin
+            data_memory_reg[write_select] <= inp;
+        end
+    end
+end
+
+endmodule
