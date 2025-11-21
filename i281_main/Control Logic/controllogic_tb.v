d4fbf029477413c5a21b145783e365c8d41c5ad8
Visharad Srivastava <visharads@outlook.com>
2025-11-21 12:21:43 +0530

    Added Documentation for OpCode Decoder and Control Logic

--- i281_main/Control Logic/controllogic_tb.v
+++ i281_main/Control Logic/controllogic_tb.v
@@ -2,40 +2,44 @@
 Testbench for Control Logic Generator.
 */
 
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
 module controllogic_tb ();
 
-    // signal instantiation
-    reg [26:0] op_in;
-    reg [3:0] flag_in;
-    wire [1:18] ctrl_out;
+  // signal instantiation
+  reg  [26:0] op_in;
+  reg  [ 3:0] flag_in;
+  wire [1:18] ctrl_out;
 
-    // UUT instantiation
-    controllogic uut (
-        .op_in (op_in),
-        .flag_in (flag_in),
-        .ctrl_out (ctrl_out)
-    );
+  // UUT instantiation
+  controllogic uut (
+      .op_in(op_in),
+      .flag_in(flag_in),
+      .ctrl_out(ctrl_out)
+  );
 
-    // Stimulus
-    integer i, j, k;
-    initial begin
+  // Stimulus
+  integer i, j, k;
+  initial begin
 
-        for (i = 0; i < 23; i++) begin  
-            op_in [22:0] = 23'b0;
-            op_in[i] = 1;
-            for (j = 0; j < 16; j++) begin op_in[26:23] = j;
-                for (k = 0; k < 16; k++) begin flag_in = k; #10; end
-            end
+    for (i = 0; i < 23; i++) begin
+      op_in[22:0] = 23'b0;
+      op_in[i] = 1;
+      for (j = 0; j < 16; j++) begin
+        op_in[26:23] = j;
+        for (k = 0; k < 16; k++) begin
+          flag_in = k;
+          #10;
         end
-        $finish;
+      end
     end
+    $finish;
+  end
 
-    // Variable Dump
-    initial begin 
-        $dumpfile ("dump.vcd");
-        $dumpvars (0, controllogic_tb);
-    end     
+  // Variable Dump
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, controllogic_tb);
+  end
 
 endmodule
