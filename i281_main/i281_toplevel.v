4e0c204ee6d1d789543550f0b0e768f239cddb91
Shridhar Patil <Shridhar.g.patil1@gmail.com>
2025-11-21 13:57:51 +0530

    Quartus integration added

---
+++ i281_main/i281_toplevel.v
@@ -0,0 +1,15 @@
+/*
+i281 Top Level Entity
+*/
+
+//module includes
+`include "ALU/alu.v"
+`include "Code Memory/codemem.v"
+`include "Control Logic/controllogic.v"
+`include "Data Memory/datamem.v"
+`include "Flags/flags.v"
+`include "OpCode Decoder/opcodedec.v"
+`include "PC/pc.v"
+`include "Registers/register.v"
+
+//
